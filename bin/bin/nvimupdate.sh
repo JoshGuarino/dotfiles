e20e5a8a18d0d46a454217339b0962849a9224a6
Josh Guarino <jguarino722@gmail.com>
2025-10-14 13:09:29 -0400

    add bin dir for custom shell scripts

---
+++ bin/bin/nvimupdate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz 
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+sudo rm nvim-linux-x86_64.tar.gz
+nvim -V1 -v
